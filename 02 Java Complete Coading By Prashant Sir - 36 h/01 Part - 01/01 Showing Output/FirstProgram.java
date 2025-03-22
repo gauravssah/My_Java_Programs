4dabd91caa53c4af4ddde7d75d2f8341fe5a2055
gauravssah <124705806+gauravssah@users.noreply.github.com>
2025-03-22 13:25:51 +0530

    java firstProgrma

---
+++ 02 Java Complete Coading By Prashant Sir - 36 h/01 Part - 01/01 Showing Output/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gaurav How Are You.");
+        System.out.print("This will print without Line Break!");
+        System.out.print("This will print in same Line.");
+    }
+}
