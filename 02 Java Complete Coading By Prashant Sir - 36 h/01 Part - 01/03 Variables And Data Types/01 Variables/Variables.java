005a2fc6b2479d384cf31f380a9b2c4826759c41
gauravssah <124705806+gauravssah@users.noreply.github.com>
2025-03-23 11:34:05 +0530

    challenge2 done

---
+++ 02 Java Complete Coading By Prashant Sir - 36 h/01 Part - 01/03 Variables And Data Types/01 Variables/Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 86;
+        int yourNumber;
+
+        System.out.println("myNumber: " + myNumber);
+        yourNumber = 20;
+        System.out.println("yourNumber: " + yourNumber);
+        yourNumber = 500;
+        System.out.println("Now- yourNumber: " + yourNumber);
+
+        // float myFloat = 46;
+        float myFloat = 46.0f;
+        System.out.println("myFloat: " + myFloat);
+
+        double doubleNum = 5.0;
+        System.out.println("doubleNum: " + doubleNum);
+
+        boolean isPresent = true;
+        System.out.println("IsPresent: " + isPresent);
+
+        String name = "Gaurav Sah";
+        System.out.println("What is Your Name: " + name);
+
+        char ch = 'G';
+        System.out.println("ch: " + ch);
+
+    }
+}
---
+++ 02 Java Complete Coading By Prashant Sir - 36 h/01 Part - 01/03 Variables And Data Types/03 Escape Sequence/EscapeSequence.java
@@ -0,0 +1,10 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Hello Gaurav, \t What is your Roll Number: ");
+        System.out.println("Hello Gaurav,\b What is your Roll Number: ");
+        System.out.println("Hello Gaurav, \n What is your Roll Number: ");
+        System.out.println("Hello Gaurav, \"What is your Roll Number\": ");
+        System.out.println("Hello Gaurav, \'What is your Roll Number\': ");
+        System.out.println("Hello Gaurav, \\What is your Roll Number\\: ");
+    }
+}
